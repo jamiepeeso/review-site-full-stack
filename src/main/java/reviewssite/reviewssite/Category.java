339efcec355c2e98ee3f9553d2e8b8157bf4dbb1
Christopher A Megown <megown1204@gmail.com>
2017-11-06 13:37:53 -0500

    monday

--- src/main/java/reviewssite/reviewssite/Category.java
+++ src/main/java/reviewssite/reviewssite/Category.java
@@ -1,6 +1,5 @@
 package reviewssite.reviewssite;
 
-import java.util.Collection;
 import java.util.Set;
 
 import javax.persistence.Entity;
--- src/main/java/reviewssite/reviewssite/ReviewRepository.java
+++ src/main/java/reviewssite/reviewssite/ReviewRepository.java
@@ -1,9 +1,7 @@
 package reviewssite.reviewssite;
 
-import org.springframework.stereotype.Component;
 import org.springframework.data.repository.CrudRepository;
 
-@Component
 public interface ReviewRepository extends CrudRepository<Review, Long> {
 
 }
